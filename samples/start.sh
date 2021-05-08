fa1e65bfc47c49603395aefba77488f3f8657eea
Parveen Chahal <mail.chahal@gmail.com>
2021-05-09 01:14:44 +0530

    Update start.sh

--- samples/start.sh
+++ samples/start.sh
@@ -10,4 +10,4 @@ files="$files;proxy-ca.key:$x"
 
 c=$(cat squid.conf | base64 -w 0)
 
-docker run -d --name squid-proxy --net host -p 3128:3128 -p 3129:3129 -p 3130:3130 -p 3131:3131 --env SQUID_CONF=$c --env FILES=$files pchahal24/squid-proxy:latest
+docker run -d --name squid-proxy --net host --env SQUID_CONF=$c --env FILES=$files pchahal24/squid-proxy:latest
