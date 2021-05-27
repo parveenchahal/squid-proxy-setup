9246d3e7b3f32534e3386fbe16b53f4ab7c3add5
Parveen Chahal <mail.chahal@gmail.com>
2021-05-27 21:17:59 +0000

    Fix for more disk consuming error

--- .docker/startup.sh
+++ .docker/startup.sh
@@ -15,4 +15,7 @@ echo $SQUID_CONF | base64 -d > "squid.conf"
 
 /usr/local/squid/sbin/squid start -f squid.conf
 
+echo "* * * * * /rotate_access_logs_and_cleanup.sh" | crontab -
+service cron restart
+
 tail -F /usr/local/squid/var/logs/access.log
