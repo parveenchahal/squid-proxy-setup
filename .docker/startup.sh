4973a761304bd0aeec61f1ef4aaf0d158df37dc6
Parveen Chahal <mail.chahal@gmail.com>
2021-10-08 13:41:35 +0000

    Updated to mount files dir

--- .docker/startup.sh
+++ .docker/startup.sh
@@ -1,19 +1,6 @@
-extract_file() {
-    IFS=':' read -ra arr <<< "$1"
-    name=${arr[0]}
-    value=${arr[1]}
-    echo $value | base64 -d > $name
-}
+cp /files/* .
 
-IFS=';' read -ra files <<< "$FILES" 
-
-for i in "${files[@]}"; do
-    extract_file $i
-done
-
-echo $SQUID_CONF | base64 -d > "squid.conf"
-
-/usr/local/squid/sbin/squid start -f squid.conf
+/usr/local/squid/sbin/squid start -f /squid.conf
 
 echo "* * * * * /rotate_access_logs_and_cleanup.sh" | crontab -
 service cron restart
--- start.sh
+++ start.sh
@@ -1,13 +1,1 @@
-x=$(cat users | base64 -w 0)
-files="users:$x"
-
-x=$(cat proxy-ca.crt | base64 -w 0)
-files="$files;proxy-ca.crt:$x"
-
-x=$(cat proxy-ca.key | base64 -w 0)
-files="$files;proxy-ca.key:$x"
-
-
-c=$(cat squid.conf | base64 -w 0)
-
-docker run -d --name squid-proxy --restart unless-stopped --net host --env SQUID_CONF=$c --env FILES=$files pchahal24/squid-proxy:latest
+docker run -d --name squid-proxy -v $(pwd)/files:/files --restart unless-stopped --net host pchahal24/squid-proxy:latest
