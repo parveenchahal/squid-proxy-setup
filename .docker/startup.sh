828c77459b7a89b3fd6e05c0fb4c8867c7be9f33
Parveen Chahal <mail.chahal@gmail.com>
2022-09-10 11:23:54 +0000

    modified the way of passing files to proxy container

--- .docker/startup.sh
+++ .docker/startup.sh
@@ -1,4 +1,31 @@
-cd /files
+function error()
+{
+    if [ ! -z "$1" ]
+    then
+      echo "$1"
+    fi
+    exit 1
+}
+
+if [ -z $SQUID_CONFIG ]
+then
+  error "SQUID_CONFIG environment variable is required."
+fi
+
+echo "$SQUID_CONFIG" | base64 -d > squid.conf
+
+files=$(echo "$FILES" | tr ";" "\n")
+for file in $files
+do
+  name="$(cut -d':' -f1 <<<"$file")"
+  content="$(cut -d':' -f2 <<<"$file")"
+  if [ -f "$name" ]
+  then
+    error "File $name already exists, please choose different name."
+  fi
+  echo "$content" | base64 -d > "$name"
+done
+
 /usr/local/squid/sbin/squid start -f squid.conf
 
 echo "* * * * * /rotate_access_logs_and_cleanup.sh" | crontab -
--- start.sh
+++ start.sh
@@ -1,1 +1,15 @@
-docker run -d --name squid-proxy -v /etc/ssl/certs:/etc/ssl/certs -v $(pwd)/files:/files --restart unless-stopped --net host pchahal24/squid-proxy:latest
+SQUID_CONFIG=$(cat files/squid.conf | base64 -w 0)
+
+files=$(ls files)
+FILES=""
+for file in $files
+do
+  if [ ! $file == "squid.conf" ]
+  then
+    content=$(cat files/$file | base64 -w 0)
+    FILES="$FILES;$file:$content"
+  fi
+done
+FILES="${FILES:1}"
+
+docker run -d --env SQUID_CONFIG=$SQUID_CONFIG --env FILES=$FILES --name squid-proxy --restart unless-stopped --net host pchahal24/squid-proxy:2022-09-11
